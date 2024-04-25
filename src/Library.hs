b9c06c254ffd780f579ee4e3e8dd558f20691dfc
Diana López Alvas <48812037+dlopezalvas@users.noreply.github.com>
2024-04-24 21:13:44 -0300

    Código inicial

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,32 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Nombre = String
+type Antiguedad = Number
+type Grado = Number
+type Clan = String
+
+data Hechicero = UnHechicero {
+    antiguedad :: Antiguedad, 
+    grado :: Grado,
+    clan :: Clan
+} deriving Show
+
+nobara :: Hechicero
+nobara = UnHechicero 1 3 "Kugisaki" 
+
+satoru :: Hechicero
+satoru = UnHechicero 15 0 "Gojo"
+
+maki :: Hechicero
+maki = UnHechicero 3 4 "Zenin" 
+
+yuji :: Hechicero 
+yuji = UnHechicero 0 1 "Itadori" 
+
+clanesPrestigiosos = ["Zenin", "Kamo", "Gojo"]
+
+type Equipo = [Hechicero]
+
+grupo :: Equipo
+grupo = [nobara, yuji, satoru, maki]
